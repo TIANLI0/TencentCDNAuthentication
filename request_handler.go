08ec77114b891204f9b9e1f7d84a92e88ac3d482
Tianli0 <507249007@qq.com>
2023-09-21 10:43:57 +0800

    🐞 fix: 修复IP异常处理

--- request_handler.go
+++ request_handler.go
@@ -26,17 +26,24 @@ func init() {
 func handleRequest(c *gin.Context) {
 
 	ip := c.Request.Header.Get("X-Forwarded-For")
-	if !isPathWhitelisted(c.Request.URL.Path) || !isRefererWhitelisted(c.Request.Referer()) || isPathBlacklisted(c.Request.URL.Path) || isRefererBlacklisted(c.Request.Referer()) || !ip_QPS(ip) {
+	if ip == "" {
+		ip = c.ClientIP()
+	}
+	if !isPathWhitelisted(c.Request.URL.Path) || !isRefererWhitelisted(c.Request.Referer()) || isPathBlacklisted(c.Request.URL.Path) || isRefererBlacklisted(c.Request.Referer()) || !ip_QPS(ip) || !QPS() {
 		c.JSON(http.StatusForbidden, gin.H{"message": "Forbidden."})
 	} else {
 
 		c.JSON(http.StatusOK, gin.H{"message": "OK.Tianli's CDN is working."})
+		fmt.Println("QPS:", ipAccessMap["QPS"].Count)
 	}
 }
 
 // IP QPS限制函数
 
 func ip_QPS(ip string) bool {
+	if ip == "" {
+		return true
+	}
 	mutex.Lock()
 	defer mutex.Unlock()
 
@@ -86,6 +93,8 @@ func QPS() bool {
 	go func() {
 		if record.Count > max_qps_int {
 			fmt.Println("QPS超过阈值，当前QPS为：", record.Count)
+			// 限制NewStopCdnDomainRequests函数的调用频率
+			time.Sleep(60 * time.Second)
 			result, _ := NewStopCdnDomainRequests(cdn_domains)
 			fmt.Println("请检查是否有人在攻击你的CDN！", result)
 		}
